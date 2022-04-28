87ae534b7fdc707ebf7fa4c908991a0f2fb7407e
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-29 03:12:18 +0600

    Started CURD operation and receiving response

--- index.js
+++ index.js
@@ -21,9 +21,15 @@ async function run() {
     try {
         await client.connect();
         const userCollection = client.db('foodExpress').collection('user');
-        const user = { name: "bodi Mahi", email: "mahi@gmail.com" };
-        const result = await userCollection.insertOne(user);
-        console.log(`user inserted with id:${result.insertedId}`);
+        // const user = { name: "bodi Mahi", email: "mahi@gmail.com" };
+        // const result = await userCollection.insertOne(user);
+        // console.log(`user inserted with id:${result.insertedId}`);
+        app.post('/user', (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            res.send({ result: 'success' });
+
+        });
     } finally {
         // await client.close();
     }
