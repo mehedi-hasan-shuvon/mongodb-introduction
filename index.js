ac693911c305e0e9049c18f095db7c1261bc8548
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-30 09:18:03 +0600

    added deleteing functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 // use middlewire 
 app.use(cors());
 app.use(express.json());
-
+const ObjectId = require('mongodb').ObjectId;
 
 //username: dbuser1
 
@@ -39,6 +39,15 @@ async function run() {
             console.log('adding new user', newUser);
             const result = await userCollection.insertOne(newUser);
             res.send(result);
+        });
+
+        //delete a user
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+
         });
 
 
